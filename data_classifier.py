d9ad79d155252bd7c1bb3fd0e7c365821dba1dfd
pegasis470 <101937683+pegasis470@users.noreply.github.com>
2022-08-14 16:10:27 +0530

    Update data_classifier.py

--- data_classifier.py
+++ data_classifier.py
@@ -1,35 +1,63 @@
 #!/usr/bin/env python3
 # importing pandas library 
 import pandas as pd
-## function is defined to take inputs and proccess them to genrate 
+import numpy as np
+
+def remove_k(df,column):
+    new_col=[]
+    for i in df[column]:
+        new_col.append(int(i.strip("k")))
+    df[column]=new_col
 def main(df):
-    global sort
-    print("Please choose column")
-    column_lst=list(df.columns)
-    column_lst.remove(column_lst[0])
-    print(column_lst)
-    column=input()
-    if column in column_lst:
-        print(f"sorting by {column} ")
-        print(list(set(df[column])))
-        print("please choose one")
-        query=input()
-        for index, row in df.iterrows():
-            if type(row[column]) == str:
-                sort=df.loc[df[column] == query]
-            if type(row[column]) == int:
-                sort=df.loc[df[column] == int(query)]
-        display(sort)
-    else:
-        print("invilaid input please choose one from displayed and try agian")
-        return None
-      
-if __name__ == "__main__":
-    df=pd.read_json("data.json")
-    main(df)
-    print("save a json file?")
+    global final_df
+    print("Enter No. of parameters:")
+    pam=int(input())
+    final_df=pd.DataFrame()
+    for i in range(pam):
+        print("Type Column Name from the following:")
+        column_list=list(df.columns.values)
+        column_list.remove(df.columns[0])
+        print(column_list)
+        column=input()
+        if type(df[column].values[0])==str:
+            print("Choose the desired value from the once below:")
+            if len(list(final_df.values))==0:
+                print(list(set(df[column].values)))
+            else:
+                print(list(set(sort[column].values)))
+            query=input()
+            sort=df.loc[df[column] == query]
+            final_df=sort
+        elif type(df[column].values[0])==np.int64:
+            print("press 1 for high to low and 2 for low to high")
+            secquence=int(input())
+            if len(list(final_df.values))==0:
+                if secquence == 1:
+                    sort=df.sort_values(by=[column], ascending=True)
+                else:
+                    sort=df.sort_values(by=[column], ascending=False)
+                final_df=sort
+
+            else:
+                if secquence == 1:
+                    sort=sort.sort_values(by=[column], ascending=True)
+                else:
+                    sort=sort.sort_values(by=[column], ascending=False)
+                final_df=sort
+
+    return final_df
+
+
+
+
+if __name__=="__main__":
+    df=pd.read_csv("~/jupyter notebooks/data/data.csv")
+    remove_k(df,"Avg. Cost($)")
+    final=main(df)
+    print(final)
+    print("save a csv file?")
     save=input()
     if save == "yes":
       print("please enter the name of file ")
       file_name=input()
-      sort.to_json(f"{file_name}.json")      
+      final.to_csv(f"{file_name}.csv")      
